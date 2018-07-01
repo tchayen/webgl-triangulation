ed270042d7a9b417ffa8d4f2af83f240350928ba
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-07-01 23:08:23 +0200

    Add basis of geometry helpers

---
+++ src/geometry.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {Object[]} points array of 2D points
+ * @returns {Object[]} array of triangles
+ */
+const triangularizeLine = points => {
+  const triangularized = []
+  return triangularized
+}
+
+export {
+  triangularizeLine,
+}
