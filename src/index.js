c9a191fc95e48c2dd67896db7f148dbca5b37b2b
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-07-01 23:21:14 +0200

    Change rendering example to letter 'F'

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-import { setUpCanvas, createShader, createProgram } from './webgl'
-import * as geo from './geo'
+import * as Utils from './webgl'
+import * as Geography from './geography'
+import * as Geometry from './geometry'
+import * as Matrix from './matrix'
 import '../public/style.scss'
 
-const krk = [[50.0, 19.85], [50.105, 20.13]]
-krk.forEach(c => {
-  const [c1, c2] = c
-  c = [c2, c1]
-  const m = geo.degreesToMeters(...c)
-  const p = geo.metersToPixels(...m, 12)
-
-  console.log(m, p)
-})
-console.log(geo.metersToPixels(...geo.degreesToMeters(19.9484548, 50.0488673), 12))
-console.log(geo.metersToPixels(...geo.degreesToMeters(19.9954434, 50.0641602), 12))
+// console.log(Geography.metersToPixels(...Geography.degreesToMeters(19.9484548, 50.0488673), 12))
+// console.log(Geography.metersToPixels(...Geography.degreesToMeters(19.9954434, 50.0641602), 12))
 
 // ...
 
@@ -25,44 +18,135 @@ const scalingFactor = window.devicePixelRatio || 1
 const vertex = require('./shaders/vertex.glsl')
 const fragment = require('./shaders/fragment.glsl')
 
-const positions = [0, 0, 0, 1.0, 1.0, 0]
+// ...
+
+const points = [[1, 2], [2, 4], [4, 3], [4, 1]]
+
+let i = 0
+while (i <= points.length - 2) {
+  const dx = points[i + 1][0] - points[i][0]
+  const dy = points[i + 1][1] - points[i][1]
+
+  const n1 = [dy, -dx]
+  const n2 = [-dy, dx]
+
+  console.log(points[i], n1, n2)
+
+  i += 1
+}
+
+// ...
+
+const setGeometry = (gl) => {
+  gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([
+          // left column
+          0, 0,
+          30, 0,
+          0, 150,
+          0, 150,
+          30, 0,
+          30, 150,
+
+          // top rung
+          30, 0,
+          100, 0,
+          30, 30,
+          30, 30,
+          100, 0,
+          100, 30,
+
+          // middle rung
+          30, 60,
+          67, 60,
+          30, 90,
+          30, 90,
+          67, 60,
+          67, 90,
+      ]),
+      gl.STATIC_DRAW);
+}
+
+let positionLocation, positionBuffer, resolutionLocation, colorLocation, matrixLocation
+
+const translation = [50, 50]
+const angleInRadians = 0
+const scale = [1, 1]
+const color = [Math.random(), Math.random(), Math.random(), 1]
+
+/**
+ *
+ * @param {Object} gl WebGL context
+ * @param {*} program linked shaders
+ */
+const setupScene = (gl, program) => {
+  positionLocation = gl.getAttribLocation(program, 'a_position')
+
+  resolutionLocation = gl.getUniformLocation(program, "u_resolution")
+  colorLocation      = gl.getUniformLocation(program, "u_color")
+  matrixLocation     = gl.getUniformLocation(program, "u_matrix")
+
+  positionBuffer = gl.createBuffer()
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+  setGeometry(gl)
+  gl.clearColor(0, 0, 0, 0)
+}
+
+/**
+ *
+ * @param {Object} gl WebGL context
+ * @param {*} program linked shaders
+ */
+const drawScene = (gl, program) => {
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.useProgram(program)
+
+  gl.enableVertexAttribArray(positionLocation)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+  const size      = 2        // 2 components per iteration, i.e. for
+                             // a {x, y, z, w} vector we provide only {x, y}, z
+                             // and w will default to 0 and 1 respectively
+  const type      = gl.FLOAT // the data is 32 bit floats
+  const normalize = false    // don't normalize the data
+  const stride    = 0        // 0 = move forward size * sizeof(type) each iteration to get the next position
+  const offset    = 0        // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset)
+
+  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height)
+
+  gl.uniform4fv(colorLocation, color)
+
+  const translationMatrix = Matrix.translation(translation[0], translation[1])
+  const rotationMatrix = Matrix.rotation(angleInRadians)
+  const scaleMatrix = Matrix.scaling(scale[0], scale[1])
+
+  // Multiply the matrices.
+  let matrix = Matrix.multiply(translationMatrix, rotationMatrix)
+  matrix = Matrix.multiply(matrix, scaleMatrix)
+
+  gl.uniformMatrix3fv(matrixLocation, false, matrix)
+
+  const count         = 18 // Execute vertex shader n times
+  const arrayOffset   = 0
+  const primitiveType = gl.TRIANGLES
+  gl.drawArrays(primitiveType, arrayOffset, count)
+}
 
 // Set ups
-const canvas = setUpCanvas(width, height, scalingFactor)
+const canvas = Utils.setUpCanvas(width, height, scalingFactor)
 
 const gl = canvas.getContext('webgl')
 if (!gl) throw 'WebGL is not supported'
 
 // Shaders
-const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertex)
-const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragment)
+const vertexShader = Utils.createShader(gl, gl.VERTEX_SHADER, vertex)
+const fragmentShader = Utils.createShader(gl, gl.FRAGMENT_SHADER, fragment)
 
-const program = createProgram(gl, vertexShader, fragmentShader)
+const program = Utils.createProgram(gl, vertexShader, fragmentShader)
 
-// Atrributes buffer
-const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
-const positionBuffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-
-gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight)
-gl.clearColor(0, 0, 0, 0)
-gl.clear(gl.COLOR_BUFFER_BIT)
-gl.useProgram(program)
-
-gl.enableVertexAttribArray(positionAttributeLocation)
-
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-const size      = 2        // 2 components per iteration, i.e. for
-                           // a {x, y, z, w} vector we provide only {x, y}, z
-                           // and w will default to 0 and 1 respectively
-const type      = gl.FLOAT // the data is 32 bit floats
-const normalize = false    // don't normalize the data
-const stride    = 0        // 0 = move forward size * sizeof(type) each iteration to get the next position
-const offset    = 0        // start at the beginning of the buffer
-gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
-
-const count         = 3 // Execute vertex shader 3 times
-const arrayOffset   = 0
-const primitiveType = gl.TRIANGLES
-gl.drawArrays(primitiveType, arrayOffset, count)
+setupScene(gl, program)
+drawScene(gl, program)
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,7 +1,20 @@
-attribute vec4 a_position;
-varying vec4 v_color;
+attribute vec2 a_position;
+
+uniform vec2 u_resolution;
+uniform mat3 u_matrix;
 
 void main() {
-  gl_Position = a_position;
-  v_color = gl_Position * 0.5 + 0.5;
+  // Multiply the position by the matrix.
+  vec2 position = (u_matrix * vec3(a_position, 1)).xy;
+
+  // convert the position from pixels to 0.0 to 1.0
+  vec2 zeroToOne = position / u_resolution;
+
+  // convert from 0->1 to 0->2
+  vec2 zeroToTwo = zeroToOne * 2.0;
+
+  // convert from 0->2 to -1->+1 (clipspace)
+  vec2 clipSpace = zeroToTwo - 1.0;
+
+  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
 }
