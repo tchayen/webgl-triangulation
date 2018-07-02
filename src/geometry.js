6d29ef4acfa89ca0c8f18d100a83f1bd893c7ab7
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-07-03 01:52:40 +0200

    Add vector module

--- src/geometry.js
+++ src/geometry.js
@@ -1,3 +1,5 @@
+import * as Vector from './vector'
+
 /**
  *
  * @param {Number[][]} points array of 2D points
@@ -6,7 +8,6 @@
  */
 const triangularizeLine = (points, width) => {
   const triangles = []
-  const addVec2 = (a, b) => [a[0] + b[0], a[1] + b[1]]
   let i = 0
   while (i <= points.length - 2) {
     const dx = points[i + 1][0] - points[i][0]
@@ -18,19 +19,103 @@ const triangularizeLine = (points, width) => {
     const n2 = [-dy / length, dx / length]
 
     triangles.push(
-      ...addVec2(points[i + 1], n2),
-      ...addVec2(points[i], n2),
-      ...addVec2(points[i], n1),
+      ...Vector.add2(points[i + 1], n2),
+      ...Vector.add2(points[i], n2),
+      ...Vector.add2(points[i], n1),
 
-      ...addVec2(points[i], n1),
-      ...addVec2(points[i + 1], n1),
-      ...addVec2(points[i + 1], n2),
+      ...Vector.add2(points[i], n1),
+      ...Vector.add2(points[i + 1], n1),
+      ...Vector.add2(points[i + 1], n2),
     )
     i += 1
   }
   return new Float32Array(triangles)
 }
 
+/**
+ * Triangularizes line
+ * @param {Number[][]} points array of 2D points
+ * @param {Number[]} width width of the line
+ * @returns {Number[]} array of triangle coordinates
+ */
+const triangularizeLineMiter = (points, width) => {
+  const triangles = []
+  let dx = [], dy = [], length = [], n = []
+
+  const addTriangles = (p1, p2, n) => {
+    triangles.push(
+      ...Vector.add2(p2, n[1]),
+      ...Vector.add2(p1, n[1]),
+      ...Vector.add2(p1, n[0]),
+
+      ...Vector.add2(p1, n[0]),
+      ...Vector.add2(p2, n[0]),
+      ...Vector.add2(p2, n[1]),
+    )
+  }
+
+  const calculateN = () => [ // uses simple calculation of 90° rotation
+    [dy[1] / length[1], -dx[1] / length[1]],
+    [-dy[1] / length[1], dx[1] / length[1]],
+  ]
+
+  const calculateLength = () => Math.sqrt(dx[1] * dx[1] + dy[1] * dy[1]) / 4.0
+
+  // Calculate first point (being an edge case)
+  dx[1] = points[1][0] - points[0][0]
+  dy[1] = points[1][1] - points[0][1]
+  length[1] = calculateLength()
+  n[1] = calculateN()
+
+  let i = 1
+  while (i < points.length - 2) {
+    dx[0] = dx[1]; dy[0] = dy[1]; length[0] = length[1], n[0] = n[1]
+
+    dx[1] = points[i + 1][0] - points[i][0]
+    dy[1] = points[i + 1][1] - points[i][1]
+
+    length[1] = calculateLength()
+    n[1] = calculateN()
+
+    const tangent = Vector.normalize(
+      Vector.add2(
+        Vector.normalize(Vector.sub2(points[i + 1], points[i])),
+        Vector.normalize(Vector.sub2(points[i], points[i - 1])),
+      )
+    )
+
+    const miter = (-tangent[1], tangent[0])
+
+    // Choice of normal is arbitrary, each of them would work
+    const miterLength = width / Vector.dot(miter, n[0][0])
+
+    triangles.push(
+      ...Vector.add2(points[i], n[0][1]),
+      ...Vector.add2(points[i - 1], n[0][1]),
+      ...Vector.add2(points[i - 1], n[0][0]),
+
+      ...Vector.add2(points[i - 1], n[0][0]),
+      ...Vector.add2(points[i], n[0][0]),
+      ...Vector.add2(points[i], n[0][1]),
+    )
+    i += 1
+  }
+
+  // Process the last two points
+  const size = points.length
+  addTriangles(points[size - 3], points[size - 2], n[1])
+
+  dx[1] = points[size - 1][0] - points[size - 2][0]
+  dy[1] = points[size - 1][1] - points[size - 2][1]
+  length[1] = calculateLength()
+  n[1] = calculateN()
+
+  addTriangles(points[size - 2], points[size - 1], n[1])
+
+  return new Float32Array(triangles)
+}
+
 export {
   triangularizeLine,
+  triangularizeLineMiter,
 }
---
+++ src/vector.js
@@ -0,0 +1,40 @@
+/**
+ * Adds two 2D vectors element-wise.
+ * @param {Number[]} a 2D vector
+ * @param {Number[]} b 2D vector
+ */
+const add2 = (a, b) => [a[0] + b[0], a[1] + b[1]]
+
+/**
+ * Subtracts two 2D vectors element-wise.
+ * @param {Number[]} a 2D vector
+ * @param {Number[]} b 2D vector
+ */
+const sub2 = (a, b) => [a[0] - b[0], a[1] - b[1]]
+
+/**
+ * Returns normalized (length == 1) 2D vector.
+ * @param {Number[]} v 2D vector
+ */
+const normalize = v => {
+  let w
+  const norm = Math.sqrt(v[0] * v[0] + v[1] * v[1])
+  if (norm != 0) {
+    w = [v[0] / norm, v[1] / norm]
+    return w
+  }
+}
+
+/**
+ * Calculates dot product of two 2D vectors.
+ * @param {Number[]} a 2D vector
+ * @param {Number[]} b 2D vector
+ */
+const dot = (a, b) => a[0] * b[0] + a[1] * b[1]
+
+export {
+  add2,
+  sub2,
+  normalize,
+  dot,
+}
